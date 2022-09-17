5e07d0a9f4cda7f860b7abbadcf8bf60c222012d
sdivakaran143 <sdivakaran683@gmail.com>
2022-09-17 23:10:19 +0530

    17-09-2022

--- Diva.js
+++ Diva.js
@@ -17,8 +17,15 @@ const Amazondetials=(producturl)=>{
         console.log("\nthe price at Amazon  : ");
         console.log("The products is : "+string.substring(0,string.indexOf("(")));
         console.log("Product spec is : "+splitStr($("#title").text()));
-        console.log("The products value is ₹"+$("#corePriceDisplay_desktop_feature_div > div.a-section.a-spacing-none.aok-align-center > span > span:nth-child(2) > span.a-price-whole").text());
-        //console.log(data);
+        if(!($("#corePriceDisplay_desktop_feature_div > div.a-section.a-spacing-none.aok-align-center > span > span:nth-child(2) > span.a-price-whole").text())){
+           console.log("The products value is "+$("#corePrice_desktop > div > table > tbody > tr:nth-child(2) > td.a-span12 > span.a-price.a-text-price.a-size-medium.apexPriceToPay > span.a-offscreen").text());
+            if(!$("#corePrice_desktop > div > table > tbody > tr:nth-child(2) > td.a-span12 > span.a-price.a-text-price.a-size-medium.apexPriceToPay > span.a-offscreen").text()){
+                console.log("Unable to Fetch The Product Price ....")
+            }
+        }
+        else{
+            console.log("The products value is ₹"+$("#corePriceDisplay_desktop_feature_div > div.a-section.a-spacing-none.aok-align-center > span > span:nth-child(2) > span.a-price-whole").text());
+        }//console.log(data);
     });
 };
 function splitStr(str) {
@@ -30,7 +37,8 @@ let fid="MOBFDHVQXCDJFDHN";
 let FlipkartLink="https://www.flipkart.com/acer-nitro-27-inch-full-hd-led-backlit-ips-panel-165-gaming-monitor-vg270s/p/itme964fae95303f?pid="+fid+"&lid=LSTMONG9R2NTE8YVHEAELDQIL&marketplace=FLIPKART&store=6bo%2Fg0i%2F9no&srno=b_1_2&otracker=hp_omu_Best%2Bof%2BElectronics_4_3.dealCard.OMU_TXZMLQJZFW8U_3&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_3_L2_view-all%2Chp_omu_PINNED_neo%2Fmerchandising_Best%2Bof%2BElectronics_NA_dealCard_cc_4_NA_view-all_3&fm=neo%2Fmerchandising&iid=48fb71ef-d12d-4b0a-a92d-e36e7ab58757.MONG9R2NTE8YVHEA.SEARCH&ppt=hp&ppn=homepage&ssid=up9wxozlj40000001663324480223";
 Flipkartdetials(FlipkartLink);
 
-//let aid="B07RCV9YKM";
-let aid="B09TWDYSWQ";
+let aid="B07RCV9YKM";
+//let aid="B07WFPMGQQ";
 let AmazonLink="https://www.amazon.in/dp/"+aid;
 Amazondetials(AmazonLink);
+
