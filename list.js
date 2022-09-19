2fab598b48c01fa7a22528709f977a9338d9ca7d
sdivakaran143 <sdivakaran683@gmail.com>
2022-09-20 00:05:07 +0530

    20-09-22

--- list.js
+++ list.js
@@ -1,13 +1,22 @@
 const axios =require("axios"),cherio=require('cheerio');
 
-const AmazonLinkFetch=(producturl)=>{
+const FlipkartLinkFetch=(producturl,list)=>{
     axios.get(producturl).then(({data})=>{
         const $ =cherio.load(data);
-        console.log($(".sg-col-inner").text());
+        for (let i = 2; i < 10; i++) {
+            let producttitle=($('#container > div > div._36fx1h._6t1WkM._3HqJxg > div._1YokD2._2GoDe3 > div:nth-child(2) > div:nth-child(' + i + ') > div > div > div > a > div._3pLy-c.row > div.col.col-7-12 > div._4rR01T').text().toLowerCase());
+            for(let j=0;j<list.length;j++){
+            if(producttitle.includes(list[j])||producttitle==''){}
+            else{break;} if(j==list.length-1){
+                console.log(i+"===========> "+producttitle);i=11;break;
+            }}
+        }  
     });
 }; 
-let productname="oneplus phone";
-productname=productname.replaceAll(" ","+");
-let producturl="https://www.amazon.in/s?k="+productname;
-
-AmazonLinkFetch(producturl);
+let Name ="hitachi 1.5 ton".toLowerCase().trim();
+let link=Name.replace(" "+"%20%20");
+let FlipkartLink="https://www.flipkart.com/search?q="+link+"&otracker=AS_Query_HistoryAutoSuggest_5_0&otracker1=AS_Query_HistoryAutoSuggest_5_0&marketplace=FLIPKART&as-show=on&as=off&as-pos=5&as-type=HISTORY";
+requirements=Name.split(" ");
+console.log(requirements);
+FlipkartLinkFetch(FlipkartLink,requirements);
+//document.querySelector("#container > div > div._36fx1h._6t1WkM._3HqJxg > div._1YokD2._2GoDe3 > div:nth-child(2) > div:nth-child(2) > div > div")
